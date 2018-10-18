f74ccf83e7b4acb66629eed90a90c9e26e080375
Michael Moussa <michael.moussa@gmail.com>
2018-10-18 12:07:51 -0400

    Fix bug involving initial creation w/no instances
    Attach individual EC2 instances using the `aws_elb_attachment` resource
    instead, making this module support both ASG and individual EC2
    attachments (or neither).

--- tests/default/minimal.tf
+++ tests/default/minimal.tf
@@ -91,6 +91,7 @@ module "clb" {
   clb_name        = "${random_string.rstring.result}-test"
   security_groups = ["${aws_security_group.test_sg1.id}", "${aws_security_group.test_sg2.id}"]
   instances       = ["${aws_instance.test01.id}", "${aws_instance.test02.id}"]
+  instances_count = 2
 
   tags = [{
     "Right" = "Said"
